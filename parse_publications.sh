38bf91e137be4ae733851e4296c04bc6652bd432
Rob Tolboom <robtolboom@gmail.com>
2023-06-12 16:58:15 +0200

    Update parse_publications.sh

--- parse_publications.sh
+++ parse_publications.sh
@@ -5,11 +5,11 @@ set -e
 curl -H "Authorization: token ${GH_BIB_TOKEN}" \
     -o content/diag.bib \
     -s \
-    -L https://raw.githubusercontent.com/AIforAnesthesiology/website-content/master/content/diag.bib
+    -L https://github.com/RobTolboom/ankitc-website/master/content/diag.bib
 
 curl -H "Authorization: token ${GH_BIB_TOKEN}" \
     -o content/fullstrings.bib \
     -s \
-    -L https://raw.githubusercontent.com/AIforAnesthesiology/website-content/master/content/fullstrings.bib
+    -L https://github.com/RobTolboom/ankitc-website/master/content/fullstrings.bib
 
 python ./bibliography/bibparser.py
